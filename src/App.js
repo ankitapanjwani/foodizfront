8cce7fcf727a31749d4b3835fcb52af8309068d9
Ankita Panjwani <ankitapan@cybage.com>
2021-02-10 12:57:24 +0530

    foodiz restaurants cards rendered

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+/* import { Card } from '@material-ui/core'; */
+import RecipeReviewCard from './card';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <RecipeReviewCard />
+      </header>
+    </div>
+  );
+}
+
+export default App;
