048d438b22f175a659953415cb043d8583796867
Ankita Panjwani <ankitapanjwani98@gmail.com>
2021-02-16 00:03:01 +0530

    past orders page modified

--- src/components/verticleTabs.jsx
+++ src/components/verticleTabs.jsx
@@ -7,7 +7,7 @@ import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 
 function TabPanel(props) {
-  const { children, value, index, ...other } = props;
+  const { children, value, index} = props;
 
   return (
     <div
@@ -15,7 +15,7 @@ function TabPanel(props) {
       hidden={value !== index}
       id={`vertical-tabpanel-${index}`}
       aria-labelledby={`vertical-tab-${index}`}
-      {...other}
+     
     >
       {value === index && (
         <Box p={3}>
@@ -26,25 +26,13 @@ function TabPanel(props) {
   );
 }
 
-TabPanel.propTypes = {
-  children: PropTypes.node,
-  index: PropTypes.any.isRequired,
-  value: PropTypes.any.isRequired,
-};
-
-function a11yProps(index) {
-  return {
-    id: `vertical-tab-${index}`,
-    'aria-controls': `vertical-tabpanel-${index}`,
-  };
-}
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
     backgroundColor: theme.palette.background.paper,
     display: 'flex',
-    height: 224,
+    height: 100,
   },
   tabs: {
     borderRight: `1px solid ${theme.palette.divider}`,
@@ -69,35 +57,16 @@ export default function VerticalTabs() {
         aria-label="Vertical tabs example"
         className={classes.tabs}
       >
-        <Tab label="Item One" {...a11yProps(0)} />
-        <Tab label="Item Two" {...a11yProps(1)} />
-        <Tab label="Item Three" {...a11yProps(2)} />
-        <Tab label="Item Four" {...a11yProps(3)} />
-        <Tab label="Item Five" {...a11yProps(4)} />
-        <Tab label="Item Six" {...a11yProps(5)} />
-        <Tab label="Item Seven" {...a11yProps(6)} />
+        <Tab label="My Profile" />
+        <Tab label="Past Orders" />
       </Tabs>
       <TabPanel value={value} index={0}>
-        Item One
+        My Profile
       </TabPanel>
       <TabPanel value={value} index={1}>
-        Item Two
+        Past Orders
       </TabPanel>
-      <TabPanel value={value} index={2}>
-        Item Three
-      </TabPanel>
-      <TabPanel value={value} index={3}>
-        Item Four
-      </TabPanel>
-      <TabPanel value={value} index={4}>
-        Item Five
-      </TabPanel>
-      <TabPanel value={value} index={5}>
-        Item Six
-      </TabPanel>
-      <TabPanel value={value} index={6}>
-        Item Seven
-      </TabPanel>
+     
     </div>
   );
 }
